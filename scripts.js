5aa94fa83a3650160af6fde5ea1c8db40d948283
Darwin Lyons <whoischarles@gmail.com>
2019-07-18 21:26:26 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+//nameSpace
+const decide = {}
+
+//global variables
+const getClass = document.getElementsByClassName("example")
+
+//document ready
+
+decide.document.addEventListener("DOMContentLoaded", function (event) {
+  decide.init()
+});
+
+//init function
+
+const init = () => {
+
+}
+
+//add inputs to the form when the user hits enter on pro
+.append('<input type="text" class="nextPro" placeholder="Type your next pro here">')
+//add inputs to the form when the user hits enter on con
+//tally pro numbers on click
+//tally con numbers on click
+//evaluate which is higher and print answer
