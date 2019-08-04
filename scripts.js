0e30085b413f719d9c28fbee7f3aee04da0f9dec
Darwin Lyons <whoischarles@gmail.com>
2019-08-04 13:04:02 -0400

    added user input to answer

--- scripts.js
+++ scripts.js
@@ -1,24 +1,76 @@
 //nameSpace
-const decide = {}
+// const decide = {};
 
-//global variables
-const getClass = document.getElementsByClassName("example")
 
 //document ready
 
-decide.document.addEventListener("DOMContentLoaded", function (event) {
-  decide.init()
+document.addEventListener("DOMContentLoaded", function (event) {
+  init()
 });
 
 //init function
 
 const init = () => {
+  listenClick()
+  // addFields();
 
 }
 
+//event click listener
+
+const listenClick = () => {
+  document.getElementById("decideButton").addEventListener("click", function (event) {
+  preventDefault()
+  getPro()
+  revealAnswer()
+}); 
+}
+
+//prevent default
+const preventDefault = () => {
+  event.preventDefault()
+}
+
+//reveal answer
+const revealAnswer = () => {
+  const question = document.getElementById('question').value
+
+  document.getElementById("answer").innerText = "On the matter of " + question + " you tallied " + "number" + " points in the pro column and " + "number" + " points in the con column, so you should";
+}
+
+//tally the numbers in the pro column
+
+const getPro = () => {
+  const proNum = document.getElementById('proNum').value
+  console.log('proNum')
+}
+
+
 //add inputs to the form when the user hits enter on pro
-.append('<input type="text" class="nextPro" placeholder="Type your next pro here">')
+
+// 
+
+// function addFields() {
+//   // Number of inputs to create
+//   const number = document.getElementById("pro").value;
+//   // Container <div> where dynamic content will be placed
+//   var container = document.getElementById("decideForm");
+ 
+//   for (i = 0; i < number; i++) {
+//     // Append a node with a random text
+//     decideForm.appendChild(document.createTextNode("Member " + (i + 1)));
+//     // Create an <input> element, set its type and name attributes
+//     const input = document.createElement("input");
+//     input.type = "text";
+//     input.name = "member" + i;
+//     container.appendChild(input);
+//     // Append a line break 
+//     container.appendChild(document.createElement("br"));
+//   }
+// }
+
 //add inputs to the form when the user hits enter on con
 //tally pro numbers on click
+
 //tally con numbers on click
 //evaluate which is higher and print answer
