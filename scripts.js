689f8f8efe266947ead5a0b9279da9e5b2df1fec
Darwin Lyons <whoischarles@gmail.com>
2019-08-11 16:24:52 -0400

    got the final pro and con calculations

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-//nameSpace
-// const decide = {};
-
-
 //document ready
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -13,24 +9,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 const init = () => {
   answerClick()
   proClick()
-  // addFields();
-
-}
-
-//global variables 
-
-// const getById = id => {
-//   document.getElementById(id)
-// }
-
-//event click listener
-
-const answerClick = () => {
-  document.getElementById("decideButton").addEventListener("click", function (event) {
-  preventDefault()
-  getPro()
-  revealAnswer()
-}); 
+  conClick()
 }
 
 //prevent default
@@ -38,41 +17,95 @@ const preventDefault = () => {
   event.preventDefault()
 }
 
+//event click listener for the answer
+
+const answerClick = () => {
+  document.getElementById("decideButton").addEventListener("click", function (event) {
+  preventDefault()
+  revealAnswer()
+  getPro()
+  getCon()
+}); 
+}
+
 //reveal answer
 const revealAnswer = () => {
   const question = document.getElementById('question').value
 
-  document.getElementById("answer").innerText = "On the matter of " + question + " you tallied " + "number" + " points in the pro column and " + "number" + " points in the con column, so you should";
+  document.getElementById("answer").innerText = "On the matter of " + question + " you tallied " + finalPro + " points in the pro column and " + "number" + " points in the con column, so you should";
 }
 
-//tally the numbers in the pro column
+//get the numbers in the pro column
+
+const talliedPro = []
+const finalPro = talliedPro[0]
 
 const getPro = () => {
-  const number = document.getElementById('proNum').value
-  console.log(number)
+  const proValues = []
+  const parentElement = document.getElementById('proContainer')
+  const proClasses = parentElement.getElementsByClassName('proNum')
+
+  for (let i = 0; i < proClasses.length; i++) {
+    const element = proClasses[i].value;
+    const proIntegers = parseInt(element)
+    proValues.push(proIntegers)
+  }
+  const addedValues = proValues.reduce((a, b) => a + b, 0)
+  talliedPro.push(addedValues)
+}
+
+//get the numbers in the con column
+
+const talliedCon = []
+
+const getCon = () => {
+  const conValues = []
+  const parentElement = document.getElementById('conContainer')
+  const conClasses = parentElement.getElementsByClassName('conNum')
+
+  for (let i = 0; i < conClasses.length; i++) {
+    const element = conClasses[i].value;
+    const conIntegers = parseInt(element)
+    conValues.push(conIntegers)
+  }
+  const addedValues = conValues.reduce((a, b) => a + b, 0)
+  talliedCon.push(addedValues)
 }
 
 
-//add inputs to the form when the user hits enter on pro
+
+
+//add as many pro or con options as the user wants
+
+const addElement = (cloneID, container) => {
+  const item = document.getElementById(cloneID)
+  const clone = item.cloneNode(true)
+  document.getElementById(container).appendChild(clone);
+}
 
 //event listener for pro column
 
 const proClick = () => {
   document.getElementById('addPro').addEventListener("click", function (event) {
     preventDefault()
-    addProDiv()
+    addElement("pro", "proContainer")
+  
   });
+
 }
 
-const addProDiv = () => {
-  const newDiv = document.createElement("div");
-  const currentDiv = document.getElementById("pro");
-  const parentDiv = document.getElementById("decideForm");
-  parentDiv.insertBefore(newDiv, currentDiv); 
-  newDiv.id = "pro3";
+//event listener for Con
+
+const conClick = () => {
+  document.getElementById('addCon').addEventListener("click", function (event) {
+    preventDefault()
+    addElement("con", "conContainer")
+
+  });
+
 }
 
-//add inputs to the form when the user hits enter on con
+
 //tally pro numbers on click
 
 //tally con numbers on click
